002d65ebe85ecc2068bcf475cf60a291284fc1cc
CuongQ <caitenkhongbitrung@gmail.com>
2018-08-28 02:23:40 +0700

    Fetching Data for Pages

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import Header from '../components/Header';
+import Layout from '../components/MyLayout'
+export default () => (
+  <div>
+    <Layout >
+    <p>This is the about page</p>
+    </Layout>
+  </div>
+)
